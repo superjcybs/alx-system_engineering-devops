58b780d2182d7033c202e4333d24b8891ab06899
JERRY BABATUNDE <jerrybabatunde1@gmail.com>
2024-08-15 00:13:27 +0000

    accept changes

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,7 +7,7 @@ def number_of_subscribers(subreddit):
     """Return: total number of subscribers on a given subreddit or return 0"""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+        "User-Agent": "My-User-Agent"
     }
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code != 200:
